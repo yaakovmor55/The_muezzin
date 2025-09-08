d11009811406922f367375e981434d0b79ffa0b3
Yaakov <y0583275590@gmail.com>
2025-09-08 17:54:39 +0300

    add the func STT

--- consumer_service/main.py
+++ consumer_service/main.py
@@ -2,16 +2,23 @@ from kafka_consumer import consumer
 from es_client import  ElasticCrud
 from ID_generator import IDGenerator
 from mongo_crud import MongoCRUD
+from stt import AudioTranscriber
 from logger import Logger
 logger = Logger.get_logger()
+
+
 
 try:
     es = ElasticCrud()
     es.create_index()
     m = MongoCRUD()
+    a = AudioTranscriber()
     for msg in consumer:
         doc = msg.value
         doc = IDGenerator.generate(doc)
+        print(doc)
+        text = a.transcribe(doc["path"])
+        doc["metadata"]["STT"] = text
         es.create_data(doc["id"], doc["metadata"])
         m.add_file(doc["path"], doc["id"])
         logger.info()
---
+++ consumer_service/stt.py
@@ -0,0 +1,13 @@
+from faster_whisper import WhisperModel
+
+class AudioTranscriber:
+    def __init__(self, model_size="tiny"):
+        self.model = WhisperModel(model_size)
+
+    def transcribe(self, file_path):
+        segments, info = self.model.transcribe(file_path)
+        text= " "
+        for segment in segments:
+            text += segment.text + "\n"
+        return text.strip()
+
