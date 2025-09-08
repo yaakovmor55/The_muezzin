0df10833dc966ccd358de76f78c4b7c75bfdd11f
Yaakov <y0583275590@gmail.com>
2025-09-08 14:57:43 +0300

    update config

---
+++ consumer_service/config.py
@@ -0,0 +1,11 @@
+PODCASTS_TOPIC = "podcasts_topic"
+BOOTSTRAP_SERVERS = 'localhost:9092'
+ELASTIC_CONN='http://localhost:9200'
+INDEX_NAME="podcasts"
+
+
+MONGO_URI = "mongodb://localhost:27017/"
+MONGO_DB = "podcasts"
+COLLECTIONS = {
+    "podcasts": "podcasts",
+}
