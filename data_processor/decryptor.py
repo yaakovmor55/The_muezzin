b9347f1cfd60ba5c774d4e5ff0cb5e16f035a54e
Yaakov <y0583275590@gmail.com>
2025-09-10 14:49:32 +0300

    Decodes coded words

--- data_processor/decryptor.py
+++ data_processor/decryptor.py
@@ -1,11 +1,16 @@
 import base64
-
+from logger import Logger
+logger = Logger.get_logger()
 
 def decode_string(encrypted_text):
-    encrypted_text = encrypted_text.encode('ascii')
-    decoded_bytes = base64.b64decode(encrypted_text)
-    decoded_string = decoded_bytes.decode('utf-8')
-    return decoded_string
+    try:
+        encrypted_text = encrypted_text.encode('ascii')
+        decoded_bytes = base64.b64decode(encrypted_text)
+        decoded_string = decoded_bytes.decode('utf-8')
+        logger.info("info: text decoding successfully")
+        return decoded_string.split(",")
+    except Exception as e:
+        logger.error(e)
 
 
 
