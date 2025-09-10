c11433f8c54264c6e12b2dc08d48e1c974ad6f6a
Yaakov <y0583275590@gmail.com>
2025-09-10 17:37:45 +0300

    update commands

--- comands.bat
+++ comands.bat
@@ -1,4 +1,15 @@
 docker-compose up -d
 
+docker rmi producer_image
+docker build -t producer_image .
+docker run producer_image
+
+docker rmi consumer_service
+docker build -t consumer_service .
+docker run consumer_service
+
+docker rmi data_processor
+docker build -t data_processor .
+docker run data_processor
 
 
