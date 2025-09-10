322acc66f35a6c5a115508fcde933cf8c62065a8
Yaakov <y0583275590@gmail.com>
2025-09-10 14:49:04 +0300

    Processes text and checks if it contains certain words.

---
+++ data_processor/text_processing.py
@@ -0,0 +1,31 @@
+
+
+class TextProcessing:
+    def __init__(self, stt, hostile_list, less_hostile_list):
+        self.stt = stt.split()
+        self.hostile_list = hostile_list
+        self.less_hostile_list = less_hostile_list
+
+    def text_classification(self):
+        x = 0
+        for  word in self.stt:
+            if word in self.hostile_list:
+                x += 2
+            if word in self.less_hostile_list:
+                x += 1
+        return x
+
+    def bds_percent(self):
+        stt_len = len(self.stt)
+
+        return round((self.text_classification() * 100) / stt_len) * 10
+
+    def get_bds_threat_level(self):
+        bds_threat_level = "none"
+        if self.bds_percent() > 49:
+            bds_threat_level = "high"
+        elif self.bds_percent() < 49:
+            bds_threat_level = "medium"
+
+        return bds_threat_level
+
